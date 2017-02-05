0ff40fc6dad754619616d3f3986c39d2de8cf031
Liz Bright <elizabeth.anne.bright@gmail.com>
2017-02-05 09:28:31 -0500

    updated image link

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <h1>Hello World</h1>
 
-<img src='/images/kitten.jpg' alt='Cute kitten'>
+<img src='/images/header-kittens.jpg' alt='Cute kitten'>
 
 <a href='https://en.wikipedia.org/wiki/Kitten'>Learn about kittens on Wikipedia</a>
